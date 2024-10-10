3673e497f33c16732568bc781bb8526d9e3468e3
Your Name <you@example.com>
2024-10-09 23:12:48 -0700

    adding-some-view

--- quera_class/panel/views.py
+++ quera_class/panel/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+import json
+import requests
+from rest_framework.generics import (ListAPIView,RetrieveAPIView,CreateAPIView,DestroyAPIView,UpdateAPIView,
+                                    ListCreateAPIView,RetrieveUpdateDestroyAPIView,)
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
+from .models import *
+from panel.permissions import *
+#from .serializers import *
 
-# Create your views here.
+
+
+
+
--- quera_class/practice/views.py
+++ quera_class/practice/views.py
@@ -1,3 +1,43 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+import json
+import requests
+from rest_framework.generics import (ListAPIView,RetrieveAPIView,CreateAPIView,DestroyAPIView,UpdateAPIView,
+                                    ListCreateAPIView,RetrieveUpdateDestroyAPIView,)
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
+from .models import *
+from panel.permissions import *
+from .serializers import *
 
-# Create your views here.
+
+class PracticeView(ListCreateAPIView):
+    queryset = Practice.objects.all()
+    serializer_class = PracticeSerializer
+
+    def get_permissions(self):
+        if self.request.method == "POST":
+            self.permission_classes = [IsTeacher , IsMentor]
+        elif self.request.method == "GET":
+            self.permission_classes = [IsAuthenticated]
+        return super(PracticeView, self).get_permissions()
+
+class PracticeEdit(RetrieveUpdateDestroyAPIView):
+    queryset = Practice.objects.all()
+    serializer_class = PracticeSerializer
+    permission_classes = [IsTeacher , IsMentor]
+
+class RankingView(ListAPIView):
+    queryset = Submission.objects.all()
+    serializer_class = SubmissionRankSerializer
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    ordering_fields = ["score_recived"]
+    permission_classes = [IsAuthenticated]
+
+
+class Grading(APIView):
+    pass 
+
+
